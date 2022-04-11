8eb06c0bbc94bb2fe74f38708ee11853395a682a
Brian <ogingabrian2017@gmail.com>
2022-04-11 13:02:18 +0300

    .

--- Project/settings.py
+++ Project/settings.py
@@ -124,8 +124,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
 
 LOGIN_URL = '/login/'
